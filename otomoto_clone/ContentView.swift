f89bf667d0a460620a902b4f452bf63f6d17ba32
adamcaak <ladamczak1997@gmail.com>
2025-07-10 20:57:58 +0200

    Initial Commit

---
+++ otomoto_clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  otomoto_clone
+//
+//  Created by Łukasz Adamczak on 10/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ otomoto_clone/otomoto_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  otomoto_cloneApp.swift
+//  otomoto_clone
+//
+//  Created by Łukasz Adamczak on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct otomoto_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
