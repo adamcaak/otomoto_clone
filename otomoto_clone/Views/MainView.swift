1907e751843cc496f39ae40177bd4bce6152dd3e
adamcaak <ladamczak1997@gmail.com>
2025-07-14 19:37:01 +0200

    Create MainView

---
+++ otomoto_clone/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  otomoto_clone
+//
+//  Created by Łukasz Adamczak on 14/07/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
