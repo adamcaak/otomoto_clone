63313b1b669206863608f9c80133050870b8e6a0
adamcaak <ladamczak1997@gmail.com>
2025-07-14 19:45:34 +0200

    Create FavoriteView

---
+++ otomoto_clone/Views/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  otomoto_clone
+//
+//  Created by Łukasz Adamczak on 14/07/2025.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
