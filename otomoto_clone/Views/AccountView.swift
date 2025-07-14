b324d0a4695a14cf3167e5ef67155cb1db5042ae
adamcaak <ladamczak1997@gmail.com>
2025-07-14 19:46:56 +0200

    Create AccountView

---
+++ otomoto_clone/Views/AccountView.swift
@@ -0,0 +1,18 @@
+//
+//  AccountView.swift
+//  otomoto_clone
+//
+//  Created by Łukasz Adamczak on 14/07/2025.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccountView()
+}
