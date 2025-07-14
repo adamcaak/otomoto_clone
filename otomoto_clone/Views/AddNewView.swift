d3a46f002c42ccd247dd1edb239e16dd7e98bc47
adamcaak <ladamczak1997@gmail.com>
2025-07-14 19:45:58 +0200

    Create AddNewView

---
+++ otomoto_clone/Views/AddNewView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewView.swift
+//  otomoto_clone
+//
+//  Created by Łukasz Adamczak on 14/07/2025.
+//
+
+import SwiftUI
+
+struct AddNewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewView()
+}
